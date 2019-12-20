58b9fe1bb7c469516f9649537329bf9d5f7f3190
Sergey Matveev <stargrave@stargrave.org>
2019-12-20 11:20:58 +0300

    Webmaster email address

--- www.mk
+++ www.mk
@@ -7,6 +7,7 @@ all: gogost.html
 gogost.html: *.texi
 	rm -f gogost.html/*.html
 	$(MAKEINFO) --html \
+		--set-customization-variable EXTRA_HEAD='<link rev="made" href="mailto:webmaster@cypherpunks.ru">' \
 		--set-customization-variable CSS_LINES='$(CSS)' \
 		--set-customization-variable SHOW_TITLE=0 \
 		--set-customization-variable USE_ACCESSKEY=0 \
